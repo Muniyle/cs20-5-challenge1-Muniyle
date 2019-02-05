a529a689f1e426d45ab0de2abd713c4a58d07627
Debbie Papasin <debbiep1@109d25pf0g1f8j7.student.cssd.ab.ca>
2019-02-05 14:32:44 -0700

    completed

--- I Am Poor/ViewController.swift
+++ I Am Poor/ViewController.swift
@@ -2,8 +2,8 @@
 //  ViewController.swift
 //  I Am Poor
 //
-//  Created by Angela Yu on 24/08/2016.
-//
+//  Created by Debbie Papasin on 2019-02-04.
+//  Copyright © 2019 Debbie Papasin. All rights reserved.
 //
 
 import UIKit
@@ -13,11 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
